bcfae372e710436db79f1ffce3d54469cf50da3d
jacek.bilski <jacek.bilski@gmail.com>
2013-05-27 14:47:25 +0200

    Scala presentation + code.

---
+++ src/main/scala/com/zooplus/jacekb/learningTime/scala/Hello.scala
@@ -0,0 +1,11 @@
+package com.zooplus.jacekb.learningTime.scala
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jacek_bilski
+ * Date: 27/05/13
+ * Time: 13:40
+ */
+object Hello {
+	def main(args: Array[String]) = println("Hello World!")
+}
