ef655d1a6fb7974c2806832ad0943e658229580a
jacek.bilski <jacek.bilski@gmail.com>
2013-12-19 12:31:39 +0100

    Error handling - presentation + some code.

--- src/main/scala/com/zooplus/jacekb/learningTime/errorHandling/ErrorHandling.scala
+++ src/main/scala/com/zooplus/jacekb/learningTime/errorHandling/ErrorHandling.scala
@@ -1,5 +1,7 @@
 package com.zooplus.jacekb.learningTime.errorHandling
 
+import scala.util.{Failure, Success, Try}
+
 /**
  * Created with IntelliJ IDEA.
  * User: jacek_bilski
@@ -17,9 +19,22 @@ object ErrorHandling {
 		else
 			Some(numbers.sum / numbers.size)
 
+	def mean3(numbers: Seq[BigDecimal]): Either[String, BigDecimal] =
+		if (numbers.size == 0)
+			Left("Sequence empty")
+		else
+			Right(numbers.sum / numbers.size)
+
+	def mean4(numbers: Seq[BigDecimal]): Try[BigDecimal] =
+		try {
+			Success(numbers.sum / numbers.size)
+		} catch {
+			case e: Exception => Failure(e)
+		}
+
 	def three(x: BigDecimal): BigDecimal = x * 3
 
 	def main(args: Array[String]): Unit = {
-		println(mean(Seq(1, 2, 3)))
+		println(mean4(Seq(1, 2, 3)))
 	}
 }
