6ef57ed33e0a9e33a6eac85780cb6e0d50efe610
jacek.bilski <jacek.bilski@gmail.com>
2014-02-07 13:50:20 +0100

    GuavaFutures now using scala.Tuple2.

--- src/main/scala/com/zooplus/jacekb/learningTime/futures/GuavaFutures.java
+++ src/main/scala/com/zooplus/jacekb/learningTime/futures/GuavaFutures.java
@@ -4,6 +4,7 @@ import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningScheduledExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.zooplus.jacekb.learningTime.akka.common.PiCalculator;
+import scala.Tuple2;
 import scala.math.BigDecimal;
 
 import java.util.Date;
@@ -27,17 +28,14 @@ public class GuavaFutures {
 //            val end = new Date().getTime
 //                    (π, end - start)
 //        }
-            final ListenableFuture<Tuple<BigDecimal, Long>> f = service.submit(new Callable<Tuple<BigDecimal, Long>>() {
+            final ListenableFuture<Tuple2<BigDecimal, Long>> f = service.submit(new Callable<Tuple2<BigDecimal, Long>>() {
                 @Override
-                public Tuple<BigDecimal, Long> call() throws Exception {
+                public Tuple2<BigDecimal, Long> call() throws Exception {
                     PiCalculator piCalculator = new PiCalculator();
                     Long start = new Date().getTime();
                     BigDecimal π = piCalculator.calculatePiFor(0, 500000000);
                     Long end = new Date().getTime();
-                    Tuple<BigDecimal, Long> tuple = new Tuple<BigDecimal, Long>();
-                    tuple.setA(π);
-                    tuple.setB(end - start);
-                    return tuple;
+                    return new Tuple2<BigDecimal, Long>(π, end - start);
                 }
             });
 //        println("Calculation of π fired up")
@@ -54,8 +52,8 @@ public class GuavaFutures {
                 public void run() {
                     try {
                         //println(s"Calculated π: ${s._1}, time: ${s._2}")
-                        Tuple<BigDecimal, Long> result = f.get();
-                        System.out.println("Calculated π: " + result.a + ", time: " + result.b);
+                        Tuple2<BigDecimal, Long> result = f.get();
+                        System.out.println("Calculated π: " + result._1() + ", time: " + result._2());
                     } catch (Exception e) {
                         // println(s"Operation failed, exception message: ${t.getMessage}")
                         System.out.println("Operation failed, exception message: " + e.getMessage());
@@ -66,32 +64,9 @@ public class GuavaFutures {
 //        Await.ready(f, Duration.Inf)
             f.get();
             System.out.println("Result calculated");
-            Thread.sleep(1000);
-            System.out.println("Slept, exiting");
+            service.shutdown();
         } catch (Exception e) {
             System.out.println("Outer operations failed, exception message: " + e.getMessage());
-        }
-    }
-
-    private static class Tuple<A, B> {
-        private A a;
-
-        private B b;
-
-        public A getA() {
-            return a;
-        }
-
-        public void setA(A a) {
-            this.a = a;
-        }
-
-        public B getB() {
-            return b;
-        }
-
-        public void setB(B b) {
-            this.b = b;
         }
     }
 }
