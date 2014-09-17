e1af555daf675705092609b7270a7cfca71088b6
Jacek Bilski <jacek.bilski@gmail.com>
2014-09-17 20:11:38 +0200

    Pi rewritten in Java.

--- src/main/java/com/zooplus/jacekb/learningTime/akka/Pi.java
+++ src/main/java/com/zooplus/jacekb/learningTime/akka/Pi.java
@@ -1,5 +1,11 @@
 package com.zooplus.jacekb.learningTime.akka;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import com.zooplus.jacekb.learningTime.akka.pi.Commons;
+import com.zooplus.jacekb.learningTime.akka.pi.Listener;
+
 /**
  * Created with IntelliJ IDEA.
  * User: jacek_bilski
@@ -7,4 +13,16 @@ package com.zooplus.jacekb.learningTime.akka;
  * Time: 09:37
  */
 public class Pi {
+
+	public static void main(String[] args) {
+		Pi pi = new Pi();
+		pi.calculate(8, 10000, 100000);
+	}
+
+	private void calculate(int nrOfWorkers, int nrOfElements, int nrOfMessages) {
+		ActorSystem system = ActorSystem.create("PiSystem");
+		ActorRef listener = system.actorOf(Props.create(Listener.class), "listener");
+		ActorRef master = system.actorOf(Props.create(Master.class, nrOfWorkers, nrOfMessages, nrOfElements, listener), "master");
+		master.tell(new Commons.Calculate(), ActorRef.noSender());
+	}
 }
